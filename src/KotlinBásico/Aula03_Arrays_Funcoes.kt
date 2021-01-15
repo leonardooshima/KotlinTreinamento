86da6664ec8d79d35cfbbfbdeb82075aa5a3e89c
leonardooshima <leonardooshima@uol.com.br>
2021-01-15 17:03:35 -0300

    Adicionando Arrays

---
+++ src/KotlinBásico/Aula03_Arrays_Funcoes.kt
@@ -0,0 +1,43 @@
+package KotlinBásico
+
+fun main() {
+    // ARRAY - armazena número fixo de valores
+
+    var array = arrayOf("SP", "RJ", "PR")
+
+    println(array[1])
+
+    array[0] = "RJ"
+    array[1] = "SP"
+
+    println(array[1])
+
+    var mix = arrayOf(1, "String", 3)
+
+    var inteiros = intArrayOf(1, 2, 3)
+
+    for(inteiros in inteiros){
+        println("Inteiro: $inteiros")
+    }
+
+    for((index, inteiros) in inteiros.withIndex()){
+        println("Index: $index - Inteiro: $inteiros")
+    }
+
+    var outrosEstados = arrayOf("PA", "BA", "SC")
+
+    var todosEstados = array + outrosEstados
+
+    for (estado in todosEstados){
+        println("Estado: $estado")
+    }
+
+    var str = "Uma String"
+    println(str[2])
+
+
+    // FUNÇÕES
+
+
+
+}
